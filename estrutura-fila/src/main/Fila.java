a4d675eb073149349bce8be453ff62430e787389
Henrique Félix da Silveira Amorim <henriquefelixdev@gmail.com>
2019-05-10 16:48:51 -0300

    Adição de comentários ao código

--- estrutura-fila/src/main/Fila.java
+++ estrutura-fila/src/main/Fila.java
@@ -1,45 +1,64 @@
 package main;
 
-public class Fila<T> {
+public class Fila<T>{
 	
+	/* Armazena os elementos presentes na fila
+	 * Seu tipo será definido no momento da criação 
+	 * da instância da classe 						*/
 	T[] pessoasNaFila;
+	
+	/* Armazena o tamnho atual da fila 
+	 * (indice do último elemento adicionado +1)		*/
 	int tamanhoFila;
 	
+	// Construtor da classe Fila inicializando as variáveis
 	@SuppressWarnings("unchecked")
 	public Fila(int tamanhoMaximo) {
 		pessoasNaFila = (T[]) new Object[tamanhoMaximo];
 		tamanhoFila = 0;
 	}
 	
+	// Insere um novo elemento na fila
 	public void add(T elemento) {
 		pessoasNaFila[tamanhoFila++] = elemento;
 	}
 	
+	// Remove o primeiro elemento da fila e o retorna
 	public T remove() {
 		
 		T objetoRemovido = pessoasNaFila[0];
 		
-		for(int i = 0; i < tamanhoFila - 1; i++) {
-			pessoasNaFila[i] = pessoasNaFila[i+1];
+		/* Verificação para evitar a tentativa de remover
+		 * um elemento em uma fila vazia 					*/
+		if(!this.empty()) {
+			
+			for(int i = 0; i < tamanhoFila - 1; i++) {
+				pessoasNaFila[i] = pessoasNaFila[i+1];
+			}
+			
+			pessoasNaFila[--tamanhoFila] = null;
 		}
-		
-		pessoasNaFila[--tamanhoFila] = null;
 		
 		return objetoRemovido;
 	}
 	
+	// Retorna o primeiro elemento da fila, mas sem removê-lo
 	public T front() {
 		return pessoasNaFila[0];
 	}
 	
+	// Retorna o tamanho atual da fila
 	public int size() {
 		return tamanhoFila;
 	}
 	
+	// Retorna se a fila está vazia
 	public boolean empty() {
 		return tamanhoFila <= 0;
 	}
 	
+	/* Retorna uma String contendo todos os elementos da fila,
+	 * listados na ordem em que estão na fila 					*/
 	@Override
 	public String toString() {
 		String lista = "[";
@@ -55,9 +74,11 @@ public class Fila<T> {
 		return lista;
 	}
 	
+	// Teste do funcionamento da fila
 	public static void main(String[] args) {
 		Fila<String> fila = new Fila<String>(10);
 		System.out.println("Fila está vazia? " + fila.empty());
+		System.out.println(fila.remove());
 		fila.add("João");
 		fila.add("Pedro");
 		fila.add("Maria");
