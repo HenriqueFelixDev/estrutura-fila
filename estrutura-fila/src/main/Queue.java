f3281d5909251db7c4e2f120d2e2a771e8b4579e
Henrique Félix da Silveira Amorim <henriquefelixdev@gmail.com>
2019-05-11 05:53:12 -0300

    Alteração do arquivo Fila.java para Queue.java, alteração do nome da classe e seus atributos

--- estrutura-fila/src/main/Queue.java
+++ estrutura-fila/src/main/Queue.java
@@ -1,42 +1,42 @@
 package main;
 
-public class Fila<T>{
+public class Queue<T>{
 	
 	/* Armazena os elementos presentes na fila
 	 * Seu tipo será definido no momento da criação 
 	 * da instância da classe 						*/
-	T[] pessoasNaFila;
+	T[] queueElements;
 	
 	/* Armazena o tamnho atual da fila 
 	 * (indice do último elemento adicionado +1)		*/
-	int tamanhoFila;
+	int queueSize;
 	
 	// Construtor da classe Fila inicializando as variáveis
 	@SuppressWarnings("unchecked")
-	public Fila(int tamanhoMaximo) {
-		pessoasNaFila = (T[]) new Object[tamanhoMaximo];
-		tamanhoFila = 0;
+	public Queue(int queueMaxSize) {
+		queueElements = (T[]) new Object[queueMaxSize];
+		queueSize = 0;
 	}
 	
 	// Insere um novo elemento na fila
 	public void add(T elemento) {
-		pessoasNaFila[tamanhoFila++] = elemento;
+		queueElements[queueSize++] = elemento;
 	}
 	
 	// Remove o primeiro elemento da fila e o retorna
 	public T remove() {
 		
-		T objetoRemovido = pessoasNaFila[0];
+		T objetoRemovido = queueElements[0];
 		
 		/* Verificação para evitar a tentativa de remover
 		 * um elemento em uma fila vazia 					*/
 		if(!this.empty()) {
 			
-			for(int i = 0; i < tamanhoFila - 1; i++) {
-				pessoasNaFila[i] = pessoasNaFila[i+1];
+			for(int i = 0; i < queueSize - 1; i++) {
+				queueElements[i] = queueElements[i+1];
 			}
 			
-			pessoasNaFila[--tamanhoFila] = null;
+			queueElements[--queueSize] = null;
 		}
 		
 		return objetoRemovido;
@@ -44,39 +44,39 @@ public class Fila<T>{
 	
 	// Retorna o primeiro elemento da fila, mas sem removê-lo
 	public T front() {
-		return pessoasNaFila[0];
+		return queueElements[0];
 	}
 	
 	// Retorna o tamanho atual da fila
 	public int size() {
-		return tamanhoFila;
+		return queueSize;
 	}
 	
 	// Retorna se a fila está vazia
 	public boolean empty() {
-		return tamanhoFila <= 0;
+		return queueSize <= 0;
 	}
 	
 	/* Retorna uma String contendo todos os elementos da fila,
 	 * listados na ordem em que estão na fila 					*/
 	@Override
 	public String toString() {
-		String lista = "[";
+		String elements = "[";
 		
-		for(int i = 0; i < tamanhoFila; i++) {
-			lista += pessoasNaFila[i];
+		for(int i = 0; i < queueSize; i++) {
+			elements += queueElements[i];
 			
-			if(i != tamanhoFila - 1) lista += ", ";
+			if(i != queueSize - 1) elements += ", ";
 		}
 		
-		lista += "]";
+		elements += "]";
 		
-		return lista;
+		return elements;
 	}
 	
 	// Teste do funcionamento da fila
 	public static void main(String[] args) {
-		Fila<String> fila = new Fila<String>(10);
+		Queue<String> fila = new Queue<String>(10);
 		System.out.println("Fila está vazia? " + fila.empty());
 		System.out.println(fila.remove());
 		fila.add("João");
